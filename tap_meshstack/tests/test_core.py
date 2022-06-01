c3539c41fed86a8bac67a18477df485d0dd56ad0
Johannes Rudolph <jrudolph@meshcloud.io>
2022-06-01 17:45:08 +0200

    test: include stub server with poetry test run

--- tap_meshstack/tests/test_core.py
+++ tap_meshstack/tests/test_core.py
@@ -1,19 +1,37 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
-import datetime
+import os
+from multiprocessing import Process
+from time import sleep
+
+from flask import Flask
+from flask import send_from_directory
 
 from singer_sdk.testing import get_standard_tap_tests
 
 from tap_meshstack.tap import TapMeshStack
 
 SAMPLE_CONFIG = {
-    "user" : "user",
-    "password": "password",
+    "auth": {
+        "username" : "user",
+        "password": "password",
+    },
     "api_url": "http://localhost:8000",
-    # "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d")
-    # TODO: Initialize minimal tap config
+    "tag_schemas": {}
 }
 
+
+def run_stub():
+    stub = Flask(__name__)
+
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    stub_dir = this_dir + "/../../stub"
+    
+    @stub.route('/api/meshobjects/<path:path>')
+    def send_report(path):
+        return send_from_directory(f"{stub_dir}/meshobjects", f"{path}.json")
+
+    stub.run(host='localhost', port=8000)
 
 # Run standard built-in tap tests from the SDK:
 def test_standard_tap_tests():
@@ -22,8 +40,13 @@ def test_standard_tap_tests():
         TapMeshStack,
         config=SAMPLE_CONFIG
     )
+
+    server = Process(target=run_stub)
+    server.start()
+    sleep(1) # this is absolutley unreliable, yet gets the job done
+    
     for test in tests:
         test()
 
-
-# TODO: Create additional tests as appropriate for your tap.
+    server.terminate()
+    server.join()
