88be397256ce6955a7428c8eea8ca2eefa451e6a
Johannes Rudolph <jrudolph@meshcloud.io>
2022-09-01 21:51:14 +0200

    feat: use decimal data types for netAmounts

--- tap_meshstack/streams.py
+++ tap_meshstack/streams.py
@@ -14,6 +14,16 @@ class MeshChargebackStatementsStream(KrakenMeshObjectStream):
     name = "meshChargebacks"
     name_singular = "meshChargeback"
 
+    def transform_record(self, record: dict):
+        super().transform_record(record)
+
+        # fix precision of netAmounts so data warehouses pipelines can work with decimal data types instead of FLOATs
+        for li in record["status"]["lineItems"]:
+            li["netAmount"] = round(li["netAmount"], 2)
+
+        for na in record["status"]["netAmounts"]:
+            na["amount"] = round(na["amount"], 2)
+
 class MeshPaymentMethodsStream(FederationMeshObjectStream):
     name = "meshPaymentMethods"
     name_singular = "meshPaymentMethod"
